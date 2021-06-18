a67d8960e1e06d9641d1d9bc55cfe537343b53bc
tellythabi <69861556+tellythabi@users.noreply.github.com>
2021-06-18 18:32:44 +0200

    Docs for stage 2
    Final touches to Level 2

--- Stage_2/index.js
+++ Stage_2/index.js
@@ -8,81 +8,118 @@ import {PointerLockControls} from "./libs/threejs/PointerLockControls.js";
 
 import * as POINT_CONTROL from "./libs/threejs/PointerLockControls.js";
 
-let scene, camera, renderer, controls, doorOne, doorTwo, doorThree ; //global variables.
-// let keyBind = {}; // key binding array
+let scene, camera, renderer, controls, doorOne, doorTwo, doorThree ; // Setting these variables as global variables.
 
 let sidewaysMovement = 0, forwardsMovement = 0; // for PointerLock Controls
 let initialTime, finalTime;
 let correctDoor; // for the random door selector
 
+/**
+ * createWorld allows us to build our scene.
+ * @returns the scene we want to display
+ */
 const createWorld = () => {
 
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth /window.innerHeight, 0.1, 200);
+    scene = new THREE.Scene(); // Creating the scene using Three.js
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth /window.innerHeight, 0.1, 200); // use of Perspective Camera to view our world
+    // positioning the camera inside the hotel model
     camera.position.set(-18, -5.9, 0.4);
 
-    // General light to scene because model will be dark
-    const ambient = new THREE.AmbientLight(0x404040, 2); //second number = how much light we want on it
-    scene.add(ambient); //ambient light will illuminate everything equally
-
-    //Adding a Directional Light
     /**
-     *
+     * General light to scene because model will be dark
+     * @type {AmbientLight}
+     * The second number represents how much light we want on it, the intensity.
+     * Ambient light will illuminate everything equally
+     */
+    const ambient = new THREE.AmbientLight(0x404040, 2);
+    scene.add(ambient);
+
+    /**
+     * Adding a Directional Light
      * @type {DirectionalLight}
      */
     const light = new THREE.DirectionalLight(0xffffff, 2);
     light.position.set(10, 10, 100); //set position of light
     scene.add(light);
 
-    renderer = new THREE.WebGLRenderer({antialias: true}); //antialiasing allows us to draw curves but disabled by default
+    /**
+     * Rendering the scene using Three.js.
+     * Antialiasing allows us to draw curves but disabled by default
+     * @type {WebGLRenderer}
+     */
+    renderer = new THREE.WebGLRenderer({antialias: true});
 
+    /**
+     * It will keep calling the animate function in the brackets
+     * appendChild will attaches renderer to page
+     */
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setAnimationLoop(animate); //keeps calling the function in the brackets
-    document.body.appendChild(renderer.domElement); //attaches renderer to page
+    renderer.setAnimationLoop(animate);
+    document.body.appendChild(renderer.domElement);
 
-    //for window resize:
+    /**
+     * Resizing the web browser window
+     * camera.aspect will change aspect ratio for camera
+     * updateProjectionMatrix will ensure calculations done using aspect ratio gets updates to new ratio
+     */
     window.addEventListener('resize', () => {
         renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth /window.innerHeight; //change aspect ratio for camera
-        camera.updateProjectionMatrix(); // ensures calculations done using aspect ratio gets updates to new ratio
+        camera.aspect = window.innerWidth /window.innerHeight;
+        camera.updateProjectionMatrix();
     });
 
-    // variable for the outer object (the hotel)
+
+    /**
+     * Creates an instance of the Hotel, calling getHotelObject from Hotel.js file
+     * This is for the variable for the outer object (the hotel)
+     * The camera looks at the hotel
+     * @type {Hotel}
+     */
     const hotel = new Hotel();
     hotel.getHotelObject().then((object) => {
         scene.add(object);
-        camera.lookAt(object.position); //camera looks at the hotel
+        camera.lookAt(object.position);
     });
 
-    /*
-        The position of all three doors
+    /**
+     * Allows u to position all the three doors, starting with the middle one (Blue Door)
+     * Resizing the door to fit into the hotel
+     * Position the door upright
+     * Position the door on the ground/floor
+     *@returns object that gets added to scene
+     * @type {BlueDoor}
      */
     const doorOne = new BlueDoor();
     doorOne.getBlueDoorObject().then((object) => {
-        object.scale.set(0.168, 0.168, 0.168); //resizing the door to fit into the hotel
+        object.scale.set(0.168, 0.168, 0.168); //
         object.position.set(-17.9, -5.7, -4.9);
-        object.rotation.x = Math.PI / 240; //positions the door upright
-        object.rotation.y = Math.PI / 2; // positions the door on the ground/floor
-        //object.rotation.z = Math.PI / 10000;
+        object.rotation.x = Math.PI / 240;
+        object.rotation.y = Math.PI / 2;
         scene.add(object);
-        //camera.lookAt(object.position);
-        // exact coordinates {x: -19.325079303920415, y: -5.7, z: -0.3341399060727056}
     });
 
-    //The door on the right hand Side
-    const doorTwo = new WoodenDoor(); // position: {x: -16.42490580900899, y: -5.7, z: -1.3672302835366057}
+
+    /**
+     * The door on the right hand Side
+     * @returns object that gets added to scene
+     * Positions the door upright
+     * @type {WoodenDoor}
+     */
+    const doorTwo = new WoodenDoor();
     doorTwo.getWoodenDoorObject().then((object) => {
         object.scale.set(0.011, 0.011, 0.011);
         object.position.set(-16.2, -6.8, -1.5);
-        object.rotation.x = Math.PI / 270; //positions the door upright
+        object.rotation.x = Math.PI / 270;
         object.rotation.y = Math.PI / -4.3;
-        // object.rotation.z = Math.PI / 90;
         scene.add(object);
 
     });
 
-    // The door on the left hand side
-
+    /**
+     * The door on the left hand side
+     * @returns object that gets added to scene
+     * @type {Door}
+     */
     const doorThree = new Door();
     doorThree.getDoorObject().then((object) => {
         object.scale.set(0.0095, 0.0095, 0.0095); //resizing the door
@@ -90,21 +127,24 @@ const createWorld = () => {
         object.rotation.x = Math.PI / 270;
         object.rotation.y = Math.PI / 3.9;
         scene.add(object);
-        //camera.lookAt(object.position);
-        // exact coordinates {x: -19.325079303920415, y: -5.7, z: -0.3341399060727056}
     });
 
-    /*
-    This following line is a random selector, meaning the correct door would not remain fixed
-    rather it will alternate between the three doors
+
+    /**
+     * This following line is a random selector, meaning the correct door would not remain fixed
+     *rather it will alternate between the three doors
+     *Generate random number between 0 and 2
+     * @type {number}
      */
-    correctDoor = Math.floor(Math.random() * 3);//generate random number between 0 and 2
+    correctDoor = Math.floor(Math.random() * 3);
     init();
-
-
 };
 
-//checking the position of the camera relative to the door
+
+/**
+ * Checking the position of the camera relative to the door
+ * Allowing the results to be displayed (i.e "block")
+ */
 const doorOutcome = () => {
 
     // door 1
@@ -158,8 +198,10 @@ const showIncorrectDoorOutCome = () => {
     document.getElementById('outcomeFail').style.display = 'block';
 }
 
-/*
-To enable keyboard movement of the PointerLock Control (WASD)
+/**
+ * To enable keyboard movement of the PointerLock Control (WASD)
+ *@return forwardsMovement
+ * @param e
  */
 function keyDowns(e) {
     switch (e.code) {
@@ -181,8 +223,9 @@ function keyDowns(e) {
     }
 }
 
-/*
-Implementing PointerLock Controls
+/**
+ * Implementing PointerLock Controls
+ * Initialises variables and on event listeners, and loads models
  */
 function init() {
 
@@ -232,7 +275,7 @@ function init() {
 initialTime = Date.now();
 
 /**
- *
+ * Enables the scene to animate and allow movement of camera as well.
  * @param time
  */
 const animate = (time) => {
